8ed4ff0aea2039ab624478be9cd581fd28d94a49
Kiattisak Phanphu <kiattisak.pa@outlook.com>
2021-04-16 15:12:46 +0700

    first commit

---
+++ comps/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return ( 
+        <footer>Copyright 2021 kiattisak</footer>
+     );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+        cillum sint consectetur cupidatat Lorem ipsum dolor sit amet, qui minim
+        labore adipisicing minim sint cillum sint consectetur cupidatat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+        cillum sint consectetur cupidatat Lorem ipsum dolor sit amet, qui minim
+        labore adipisicing minim sint cillum sint consectetur cupidatat.
+      </p>
+    </div>
+  );
+};
+
+export default About;
