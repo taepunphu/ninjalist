0d583dc9586b6f12487a9c93855607749f07d4f9
Kiattisak Phanphu <kiattisak.pa@outlook.com>
2021-05-12 07:33:51 +0700

    dynamic routes

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,26 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-        cillum sint consectetur cupidatat Lorem ipsum dolor sit amet, qui minim
-        labore adipisicing minim sint cillum sint consectetur cupidatat.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-        cillum sint consectetur cupidatat Lorem ipsum dolor sit amet, qui minim
-        labore adipisicing minim sint cillum sint consectetur cupidatat.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"></meta>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+          cillum sint consectetur cupidatat Lorem ipsum dolor sit amet, qui
+          minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+          cillum sint consectetur cupidatat Lorem ipsum dolor sit amet, qui
+          minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,32 @@
 import Head from "next/head";
 import Link from "next/link";
 import Footer from "../comps/Footer.js";
-import Navbar from '../comps/Navbar.js'
+import Navbar from "../comps/Navbar.js";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-        cillum sint consectetur cupidatat.
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-        cillum sint consectetur cupidatat.
-      </p>
-      <Link href="/ninjas"><a className={styles.btn}>See Ninjas Listing</a></Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"></meta>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+          cillum sint consectetur cupidatat.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+          cillum sint consectetur cupidatat.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninjas Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
